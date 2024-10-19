151e22c74950297ad46dea2698b16f649326c3dc
LimDong <ilhai6@naver.com>
2024-10-20 02:03:18 +0900

    mousemove 이벤트 주석 완료

--- src/main.ts
+++ src/main.ts
@@ -46,26 +46,32 @@ const arrowHelper = new THREE.ArrowHelper();
 arrowHelper.setLength(0.5);
 scene.add(arrowHelper);
 
+// raycaster는 화면상에서 발생하는 이벤트를 기반으로 광선을 발사해서 그 광선이 어떤 객체와 교차하는지 계산하는 기능을 제공한다.
 renderer.domElement.addEventListener("mousemove", (e) => {
-  // -1과 1사이의 좌표 표시
+  // 마우스 좌표를 NDC 좌표로 변환
   mouse.set(
     (e.clientX / renderer.domElement.clientWidth) * 2 - 1,
     -(e.clientY / renderer.domElement.clientHeight) * 2 + 1
   );
 
+  // 카메라를 기준으로 광선을 발사
   raycaster.setFromCamera(mouse, camera);
 
+  // 교차한 객체 정보 가져오기
   const intersects = raycaster.intersectObjects(pickables, false);
 
+  // 교차한 객체가 있을 경우
   if (intersects.length) {
-    // console.log(intersects);
+    console.log(intersects);
     // console.log(intersects[0].point);
     // console.log(intersects[0].object.name + " " + intersects[0].distance);
     // console.log((intersects[0].face as THREE.Face).normal);
     const n = new THREE.Vector3();
+    // 교차하는 물체가 향하는 방향을 복사함
     n.copy((intersects[0].face as THREE.Face).normal);
     //     //n.transformDirection(intersects[0].object.matrixWorld)
     arrowHelper.setDirection(n);
+    // 마우스 포인터가 가리키고 있는 객체 표면의 정확한 교차 지점
     arrowHelper.position.copy(intersects[0].point);
   }
 });
